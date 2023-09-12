5cebc0d700f5890b3e96867cfd577a3c3e4db3e8
DESKTOP-RVHMU4K\powal <powalka.dominika@gmail.com>
2023-09-12 14:07:19 +0200

    Dodanie przycisku powrotu do dashboardu głównego

--- app/src/main/java/com/example/auth/HeartRateMeasurementActivity.java
+++ app/src/main/java/com/example/auth/HeartRateMeasurementActivity.java
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class HeartRateMeasurementActivity extends Activity {
     @SuppressLint("SetTextI18n")
@@ -15,5 +18,16 @@ public class HeartRateMeasurementActivity extends Activity {
         // Ustawiamy nagłówek
         TextView headerTextView = findViewById(R.id.headerTextView);
         headerTextView.setText("Wykonaj pomiar tetna");
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tutaj dodaj kod do obsługi powrotu do dashboardu głównego
+                // Na przykład, możesz użyć Intent do przejścia do dashboardu głównego.
+                Intent intent = new Intent(HeartRateMeasurementActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
