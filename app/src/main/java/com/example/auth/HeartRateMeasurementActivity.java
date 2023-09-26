fa3a6184f4ce4234704d71ea42a7eea27940d171
DESKTOP-RVHMU4K\powal <powalka.dominika@gmail.com>
2023-09-26 19:01:55 +0200

    Wprowadzono modyfikacje w pliku HeartRateMeasurement.java i w plikach build.gradle w celu integracji aplikacji z Google Drive API

--- app/src/main/java/com/example/auth/HeartRateMeasurementActivity.java
+++ app/src/main/java/com/example/auth/HeartRateMeasurementActivity.java
@@ -9,34 +9,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.drive.CreateFileActivityOptions;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.drive.DriveClient;
-import com.google.android.gms.drive.DriveContents;
-import com.google.android.gms.drive.DriveFile;
-import com.google.android.gms.drive.DriveFolder;
-import com.google.android.gms.drive.MetadataChangeSet;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.IOException;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -48,8 +52,7 @@ public class HeartRateMeasurementActivity extends Activity {
     private RecyclerView recyclerView;
     private MeasurementAdapter adapter;
     private GoogleSignInClient googleSignInClient;
-    private DriveClient driveClient;
-    private DriveFolder appFolder;
+    private Drive driveService;
 
     private static final int REQUEST_CODE_SIGN_IN = 1;
 
@@ -116,7 +119,6 @@ public class HeartRateMeasurementActivity extends Activity {
     }
 
     private void signInToGoogleDrive() {
-        // Rozpocznij proces autoryzacji Google Drive po kliknięciu przycisku "POBIERZ"
         Intent signInIntent = googleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
     }
@@ -127,8 +129,7 @@ public class HeartRateMeasurementActivity extends Activity {
 
         if (requestCode == REQUEST_CODE_SIGN_IN) {
             if (resultCode == RESULT_OK) {
-                // Autoryzacja w Google Drive zakończona sukcesem
-                handleDriveSignInSuccess();
+                handleDriveSignInSuccess(data);
             } else {
                 Log.e("GoogleDriveAuth", "Google Drive sign-in failed.");
                 Toast.makeText(getApplicationContext(), "Błąd autoryzacji Google Drive.", Toast.LENGTH_SHORT).show();
@@ -136,75 +137,69 @@ public class HeartRateMeasurementActivity extends Activity {
         }
     }
 
-    private void handleDriveSignInSuccess() {
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null) {
-            String jsonFileName = "measurements_" + generateMeasurementId() + ".json";
-            String jsonData = createJsonData(adapter.getMeasurements());
-
-            MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
-                    .setTitle(jsonFileName)
-                    .setMimeType("application/json")
-                    .setStarred(false)
-                    .build();
-
-            Drive.getDriveResourceClient(this, account) // Użyj konta autoryzacji
-                    .createContents()
-                    .addOnSuccessListener(new OnSuccessListener<DriveContents>() {
-                        @Override
-                        public void onSuccess(DriveContents driveContents) {
-                            OutputStream outputStream = driveContents.getOutputStream();
-                            try {
-                                outputStream.write(jsonData.getBytes());
-                                outputStream.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-
-                            Drive.getDriveResourceClient(HeartRateMeasurementActivity.this, account)
-                                    .getRootFolder()
-                                    .addOnSuccessListener(new OnSuccessListener<DriveFolder>() {
-                                        @Override
-                                        public void onSuccess(DriveFolder driveFolder) {
-                                            appFolder = driveFolder;
-
-                                            Drive.getDriveResourceClient(HeartRateMeasurementActivity.this, account)
-                                                    .createFile(appFolder, changeSet, driveContents)
-                                                    .addOnSuccessListener(new OnSuccessListener<DriveFile>() {
-                                                        @Override
-                                                        public void onSuccess(DriveFile driveFile) {
-                                                            Log.d("GoogleDriveSave", "File saved to Google Drive.");
-                                                            Toast.makeText(getApplicationContext(), "Plik JSON został zapisany w Google Drive.", Toast.LENGTH_SHORT).show();
-                                                        }
-                                                    })
-                                                    .addOnFailureListener(new OnFailureListener() {
-                                                        @Override
-                                                        public void onFailure(@NonNull Exception e) {
-                                                            Log.e("GoogleDriveSave", "Error saving file to Google Drive: " + e.getMessage());
-                                                            Toast.makeText(getApplicationContext(), "Błąd przy zapisywaniu pliku w Google Drive.", Toast.LENGTH_SHORT).show();
-                                                        }
-                                                    });
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Log.e("GoogleDriveFolder", "Error accessing root folder in Google Drive.");
-                                            Toast.makeText(getApplicationContext(), "Błąd dostępu do folderu głównego w Google Drive.", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
+    private void handleDriveSignInSuccess(Intent data) {
+        GoogleSignIn.getSignedInAccountFromIntent(data)
+                .addOnSuccessListener(new OnSuccessListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onSuccess(GoogleSignInAccount googleSignInAccount) {
+                        HttpTransport httpTransport;
+                        try {
+                            httpTransport = com.google.api.client.googleapis.javanet.GoogleNetHttpTransport.newTrustedTransport();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return;
                         }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.e("GoogleDriveSave", "Error creating file contents: " + e.getMessage());
-                            Toast.makeText(getApplicationContext(), "Błąd przy tworzeniu zawartości pliku.", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+
+                        GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                                HeartRateMeasurementActivity.this, Collections.singleton(DriveScopes.DRIVE_FILE));
+                        credential.setSelectedAccount(googleSignInAccount.getAccount());
+
+                        // Używamy GsonFactory zamiast JacksonFactory
+                        JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+                        driveService = new Drive.Builder(httpTransport, JSON_FACTORY, credential)
+                                .setApplicationName("Your App Name")
+                                .build();
+
+                        saveDataToDrive();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("GoogleDriveAuth", "User not signed in to Google Drive.");
+                        Toast.makeText(getApplicationContext(), "Użytkownik nie jest zalogowany do Google Drive.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void saveDataToDrive() {
+        List<Measurement> measurements = adapter.getMeasurements();
+        String jsonData = createJsonData(measurements);
+
+        File file = null;
+
+        File fileMetadata = new File();
+        fileMetadata.setName("measurements_" + generateMeasurementId() + ".json");
+        fileMetadata.setMimeType("application/json");
+
+        com.google.api.client.http.ByteArrayContent content = new com.google.api.client.http.ByteArrayContent("application/json", jsonData.getBytes());
+
+        try {
+            file = driveService.files().create(fileMetadata, content)
+                    .setFields("id")
+                    .execute();
+        } catch (IOException e) {
+            Log.e("GoogleDriveSave", "Error saving file to Google Drive: " + e.getMessage());
+            Toast.makeText(getApplicationContext(), "Błąd podczas zapisywania pliku na Dysku Google Drive.", Toast.LENGTH_SHORT).show();
+        }
+
+        if (file != null) {
+            Log.d("GoogleDriveSave", "File ID: " + file.getId());
+            Toast.makeText(getApplicationContext(), "Plik został zapisany w Dysku Google Drive.", Toast.LENGTH_SHORT).show();
         } else {
-            Log.e("GoogleDriveAuth", "User not signed in to Google Drive.");
-            Toast.makeText(getApplicationContext(), "Użytkownik nie jest zalogowany do Google Drive.", Toast.LENGTH_SHORT).show();
+            Log.e("GoogleDriveSave", "Error saving file to Google Drive.");
+            Toast.makeText(getApplicationContext(), "Błąd podczas zapisywania pliku na Dysku Google Drive.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -229,42 +224,29 @@ public class HeartRateMeasurementActivity extends Activity {
     }
 
     private void saveMeasurementsToFirebase() {
-        // Pobierz dane pomiarowe z adaptera
         List<Measurement> measurements = adapter.getMeasurements();
 
-        // Wyświetl dane w logach, aby sprawdzić, czy są poprawne
         for (Measurement measurement : measurements) {
             Log.d("TAG", "Tetno: " + measurement.getTetnoValue() + ", Glukoza: " + measurement.getGlukozaValue());
         }
 
-        // Pobierz aktualnie zalogowanego użytkownika
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         if (user != null) {
-            // Pobierz identyfikator użytkownika
             String userId = user.getUid();
-
-            // Stworzenie referencji do konkretnej sekcji pomiarowej w bazie danych
             DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
             DatabaseReference userReference = databaseReference.child("users").child(userId);
             DatabaseReference measurementsReference = userReference.child("measurements");
-
-            // Tworzenie unikalnego identyfikatora dla pomiaru (na podstawie znacznika czasowego)
             String measurementId = generateMeasurementId();
-
-            // Tworzenie nowej sekcji pomiarowej
             DatabaseReference newMeasurementReference = measurementsReference.child(measurementId);
 
-            // Zapisywanie pomiarów jako osobne obiekty w bazie danych
             for (int i = 0; i < measurements.size(); i++) {
                 Measurement measurement = measurements.get(i);
                 newMeasurementReference.child("tetno" + (i + 1)).setValue(measurement.getTetnoValue());
                 newMeasurementReference.child("glukoza" + (i + 1)).setValue(measurement.getGlukozaValue());
             }
 
-            // Wyświetl informację o zapisaniu danych
             Toast.makeText(getApplicationContext(), "Dane pomiarowe zostały zapisane", Toast.LENGTH_SHORT).show();
         }
     }
-
 }
